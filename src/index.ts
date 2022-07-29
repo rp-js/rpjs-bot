14820c2b1628840a1229670701a8227884ba9217
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-07-29 03:21:12 -0300

    feat: adiciona firebase ao projeto

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { config } from "dotenv";
 import { commandsDeploy } from "./commands-deploy";
+import initializeFirebase from "./firebase/admin";
 import { startBot } from "./server";
 
 config();
 
+initializeFirebase();
 commandsDeploy();
 startBot();
