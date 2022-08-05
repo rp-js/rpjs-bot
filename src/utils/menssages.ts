e570f5a8fb3e31da45c5725aacd63377f3868f13
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-08-05 00:57:31 -0300

    feat: cria funções para criar e verificar reações da messagem de reação

--- src/utils/menssages.ts
+++ src/utils/menssages.ts
@@ -2,21 +2,83 @@ import { Message, TextChannel } from "discord.js";
 import { client } from "..";
 
 export async function reactionRoles() {
-  const message = await sendMessage("1002309210148712470", "teste menssagem");
+  const messageText = "reage aí";
+  const reactions: string[] = ["teste", "testando"];
+
+  const guilds = await client.guilds.cache.get("1002309209699917894");
+  const channel = guilds?.channels.cache.get(
+    "1002309210148712470"
+  ) as TextChannel;
+
+  await channel.messages.fetch("1004958653222223872");
+
+  const message = await channel.messages.cache.get("1004958653222223872");
 
   if (!message) return;
 
-  await reactMessage("testando", message);
+  await message.reactions.removeAll();
+
+  const sended = true;
+
+  if (!sended) {
+    createReactionMessage("1002309210148712470", messageText, reactions);
+    return;
+  }
+
+  verifyReactions(message, reactions);
 }
 
-export async function sendMessage(channelId: string, messageString: string) {
+export async function createReactionMessage(
+  channelId: string,
+  messageText: string,
+  reactions: string[]
+) {
+  try {
+    const message = await sendMessage(channelId, messageText);
+
+    await Promise.all(
+      reactions.map(async (reaction) => {
+        await reactMessage(reaction, message);
+      })
+    );
+
+    return message;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function verifyReactions(
+  message: Message,
+  reactionsName: string[]
+) {
+  await Promise.all(
+    reactionsName.map(async (reactionName) => {
+      if (
+        !message.reactions.cache.find(
+          (react) => react.emoji.name === reactionName
+        )
+      ) {
+        await reactMessage(reactionName, message);
+      }
+    })
+  );
+
+  return true;
+}
+
+export async function sendMessage(channelId: string, messageText: string) {
   const channel = client.channels.cache.get(channelId) as TextChannel;
 
   if (!channel) {
     throw new Error("o canal escolhido não existe");
   }
 
-  const message = await channel.send(messageString);
+  const message = await channel.send(messageText);
+
+  if (!message) {
+    throw new Error(`Menssagem "${messageText}", não enviada`);
+  }
 
   return message;
 }
